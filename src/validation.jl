1c44c4ebf39bf9eaf00c3a0ff7e63800cd072539
Nikhil Yewale <yewale.nikhil65@gmail.com>
2021-02-16 15:29:15 +0530

    Update validation.jl

--- src/validation.jl
+++ src/validation.jl
@@ -65,66 +65,6 @@ rx = [];              # empty-reaction vector
 end
 rs = ReactionSystem(rx, t, X, k);
 
-##  Solving the reaction using different DiffEqJump solver
-#= references and their solvers for JumpProblem
-Direct()
-Gillespie, Daniel T. (1976). A General Method for Numerically Simulating
-the Stochastic Time Evolution of  Coupled Chemical Reactions.
-Journal of Computational Physics. 22 (4): 403–434.
-doi:10.1016/0021-9991(76)90041-3.
-
-
-RDirect() --> No reference
-DirectCR()
-•    A. Slepoy, A.P. Thompson and S.J. Plimpton, A constant-time kinetic Monte Carlo algorithm for
-    simulation of large biochemical reaction networks, Journal of Chemical Physics, 128 (20), 205101
-    (2008). doi:10.1063/1.2919546
-
-•    S. Mauch and M. Stalzer, Efficient formulations for exact stochastic simulation of chemical
-    systems, ACM Transactions on Computational Biology and Bioinformatics, 8 (1), 27-35 (2010).
-    doi:10.1109/TCBB.2009.47
-
-DirectFW()
-Gillespie, Daniel T. (1976). A General Method for Numerically Simulating the Stochastic Time Evolution of
-Coupled Chemical Reactions. Journal of Computational Physics. 22 (4): 403–434.
-doi:10.1016/0021-9991(76)90041-3.
-
-NRM()
-M. A. Gibson and J. Bruck, Efficient exact stochastic simulation of chemical
-systems with many species and many channels,
-Journal of Physical Chemistry A, 104 (9), 1876-1889 (2000). doi:10.1021/jp993732q
-
-FRM()
-Gillespie, Daniel T. (1976). A General Method for Numerically Simulating the Stochastic Time Evolution of
-Coupled Chemical Reactions. Journal of Computational Physics. 22 (4): 403–434.
-doi:10.1016/0021-9991(76)90041-3.
-
-FRMFW()
-Gillespie, Daniel T. (1976). A General Method for Numerically Simulating the Stochastic Time Evolution of
-Coupled Chemical Reactions. Journal of Computational Physics. 22 (4): 403–434.
-doi:10.1016/0021-9991(76)90041-3.
-
-RSSACR()
-V. H. Thanh, R. Zunino, and C. Priami, Efficient Constant-Time Complexity Algorithm for Stochastic
- Simulation of Large Reaction Networks, IEEE/ACM Transactions on Computational Biology and Bioinformatics,
- Vol. 14, No. 3, 657-667 (2017).
-
-RSSA()
-•    V. H. Thanh, C. Priami and R. Zunino, Efficient rejection-based simulation of biochemical
-    reactions with stochastic noise and delays, Journal of Chemical Physics, 141 (13), 134116 (2014).
-    doi:10.1063/1.4896985
-
-•    V. H. Thanh, R. Zunino and C. Priami, On the rejection-based algorithm for simulation and analysis
-    of large-scale reaction networks, Journal of Chemical Physics, 142 (24), 244106 (2015).
-    doi:10.1063/1.4922923
-
-SortingDirect()
-J. M. McCollum, G. D. Peterson, C. D. Cox, M. L. Simpson and N. F. Samatova,
-The sorting direct method for stochastic simulation of biochemical systems with
-varying reaction execution behavior,
-Computational Biology and Chemistry, 30 (1), 39049 (2006).
-doi:10.1016/j.compbiolchem.2005.10.007
-=#
 #=
 odesys = convert(ODESystem, rs; combinatoric_ratelaws = true);
 oprob = ODEProblem(odesys, u₀map, tspan, pₘₐₚ; parallel = true)
