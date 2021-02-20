ba09b804ff9abf51ed6c43887ddd9839cc8f4d00
Nikhil Yewale <yewale.nikhil65@gmail.com>
2021-02-20 20:33:34 +0530

    Update validation.jl

--- src/validation.jl
+++ src/validation.jl
@@ -1,17 +1,17 @@
-# doing validation with analytical expression
 using ModelingToolkit, LinearAlgebra
 using DiffEqBase ,DiffEqJump, OrdinaryDiffEq
 using LoopVectorization, Plots
 using BenchmarkTools
-using SpecialFunctions
+using SpecialFunctions, Statistics
 plotly()
-## Parameter
-N = 5;      # Number of clusters
-C  = 238.720;  # initial concentration == number of initial monomers/ volume of the bulk system
-uₒ = 10000; # No. of singlets initially
-V = uₒ/C;   # Bulk volume of system..use this in ReactionSystem
 
-Vₒ = 4.189e-09;  # volume of a singlet with 10 μm dia
+## Parameter
+N = 5;         # Number of clusters
+Vₒ = (4π/3)*(10e-06*100)^3;  # volume of a singlet in cm³
+Nₒ = 1e-06/Vₒ;   # initial conc. = (No. of init. monomers) / Volume of the bulk system
+uₒ = 10000;      # No. of singlets initially
+V = uₒ/Nₒ;       # Bulk volume of system in cm³
+
 integ(x) = Int(floor(x));
 n = integ(N/2);
 nr = N%2 == 0 ? (n*(n + 1) - n) : (n*(n + 1)); # No. of forward reactions
@@ -23,21 +23,22 @@ end
 pair = vcat(pair...);
 vᵢ = @view pair[:,1];  # Reactant 1 index
 vⱼ = @view pair[:,2];  # Reactant 2 index
-volᵢ = Vₒ*vᵢ;    # cm⁻³
-volⱼ = Vₒ*vⱼ;    # cm⁻³
-sum_vᵢvⱼ = @. vᵢ + vⱼ;  # Product index
+volᵢ = Vₒ*vᵢ;
+volⱼ = Vₒ*vⱼ;
+sum_vᵢvⱼ = @. vᵢ + vⱼ; # Product index
 
 i = parse(Int, input("Enter 1 for additive kernel,
-                2 for Multiplicative, 3 for constant"))
+        2 for Multiplicative, 3 for constant"))
+
 if i==1
-    B = 1.53e03;    # s⁻¹
+    B = 1.53e03;     # s⁻¹
     kₛ = @. B*(volᵢ + volⱼ)/V;
 elseif i==2
-    b = 3.8e11;     #  cm⁻³ s⁻¹
+    b = 3.80e11;     #  cm⁻³ s⁻¹
     kₛ = @. b*(volᵢ*volⱼ)/V;
 else
-    C = 1.84e-04;   # cm³ s⁻¹
-    kₛ = @. C/V;
+    C = 1.80e-04;    #  cm³ s⁻¹
+    kₛ = fill(C/V ,nr);
 end
 
 ## Writing-off the parameter in Pairs in Sequence
@@ -52,61 +53,73 @@ else
 end
 u₀ = zeros(Int64, N);   u₀[1] = uₒ;   # initial condition of monomers
 u₀map = Pair.(X, u₀); # population of other polymers in zeros
-rx = [];              # empty-reaction vector
 
 ##  Forming ReactionSystem
+rx = [];              # empty-reaction vector
+reactant_stoich = Array{Array{Pair{Int64,Int64},1},1}(undef, nr);
+net_stoich = Array{Array{Pair{Int64,Int64},1},1}(undef, nr);
 @time for n = 1:nr
     if (vᵢ[n] == vⱼ[n])    # checking the reactants
         push!(rx, Reaction(2*k[n], [ X[vᵢ[n]] ] ,[ X[sum_vᵢvⱼ[n]] ] ,[2],[1]));
+
+        reactant_stoich[n] = [vᵢ[n] => 2];
+        net_stoich[n] = [vᵢ[n] => -2, sum_vᵢvⱼ[n] => 1];
     else
         push!(rx, Reaction(k[n], [ X[vᵢ[n]] , X[vⱼ[n]] ] ,[ X[sum_vᵢvⱼ[n]] ],
                                 [1, 1],[1]));
+
+        reactant_stoich[n] = [vᵢ[n] => 1 , vⱼ[n] => 1];
+        net_stoich[n] = [vᵢ[n] => -1 , vⱼ[n] => -1 , sum_vᵢvⱼ[n] => 1];
     end
 end
 rs = ReactionSystem(rx, t, X, k);
 
+##  Solving the reaction using different DiffEqJump solver
 #=
 odesys = convert(ODESystem, rs; combinatoric_ratelaws = true);
 oprob = ODEProblem(odesys, u₀map, tspan, pₘₐₚ; parallel = true)
 osol = solve(oprob, Tsit5())
 =#
-
 jumpsys = convert(JumpSystem, rs; combinatoric_ratelaws = true);
 dprob = DiscreteProblem(jumpsys, u₀map, tspan, pₘₐₚ; parallel = true);
-alg = RSSA();
-jprob = @btime JumpProblem(jumpsys, dprob, alg);
-jsol = @btime solve(jprob, SSAStepper());
+alg = Direct();
+stepper = SSAStepper();
+mass_act_jump = MassActionJump(kₛ ,reactant_stoich, net_stoich);
+jprob = @btime JumpProblem(dprob, alg ,mass_act_jump ,save_positions=(false,false));
+jsol = @btime solve(jprob, stepper, saveat = 1.);
 
 ## Results for first three polymers
-v_res = [1;2;3]
-plot(jsol , lw = 2, vars = v_res ,xlabel = "Time (sec)" )
-
-## comparsion with analytical solution
+v_res = [1 ;2 ;3]         # comparsion with analytical solution
+plot(jsol)
 if i == 1
-    ϕ = @. 1 - exp(-0.00153*jsol.t) # normalised "time"
+    ϕ = @. 1 - exp(-B*Nₒ*Vₒ*jsol.t);        # normalised "time"
     sol = zeros(length(v_res), length(ϕ))
     for j in v_res
-         sol[j,:] = @. uₒ*(1 - ϕ)*(((j*ϕ)^(j-1))/gamma(j+1))*exp(-j*ϕ);
+        sol[j,:] = @. Nₒ*(1 - ϕ)*(((j*ϕ)^(j-1))/gamma(j+1))*exp(-j*ϕ);
     end
 elseif i == 2
-    ϕ = @. (0.00159*jsol.t);   # normalised "time"
+    ϕ = @. (b*Nₒ*Vₒ*Vₒ*jsol.t);             # normalised "time"
     sol = zeros(length(v_res), length(ϕ))
     for j in v_res
-        sol[j,:] = @. uₒ*(((j*ϕ)^(j-1))/(j*gamma(j+1)))*exp(-j*ϕ);
+        sol[j,:] = @. Nₒ*(((j*ϕ)^(j-1))/(j*gamma(j+1)))*exp(-j*ϕ);
     end
 else
-    ϕ = @. (0.0429*jsol.t);  # normalised "time"
+    ϕ = @. (C*Nₒ*jsol.t);                   # normalised "time"
     sol = zeros(length(v_res), length(ϕ))
     for j in v_res
-        sol[j,:] = @. 4uₒ*((ϕ^(j-1))/((ϕ + 2)^(j+1)));
+        sol[j,:] = @. 4Nₒ*((ϕ^(j-1))/((ϕ + 2)^(j+1)));
     end
 end
 # plotting normalised concentration vs analytical solution
-plot(ϕ, (jsol[1,:]), lw = 2, xlabel = "Time (sec)")
-plot!(ϕ, sol[1,:], lw = 2, line = (:dot, 4) ,label = "Analytical sol")
+plot(ϕ, jsol[1,:]/uₒ, lw = 2, xlabel = "Time (sec)",label = string("X",1),
+    title = "Constant kernel")
+plot!(ϕ, sol[1,:]/Nₒ, lw = 2, line = (:dot, 4) ,
+        label = string("Analytical sol", "--X",1))
 
-plot(ϕ, (jsol[2,:]), lw = 2, xlabel = "Time (sec)")
-plot!(ϕ, sol[2,:], lw = 2, line = (:dot, 4) ,label = "Analytical sol")
+plot!(ϕ, jsol[2,:]/uₒ, lw = 2, xlabel = "Time (sec)",label = string("X",2))
+plot!(ϕ, sol[2,:]/Nₒ, lw = 2, line = (:dot, 4) ,
+        label = string("Analytical sol", "--X",2))
 
-plot(ϕ, (jsol[3,:]), lw = 2, xlabel = "Time (sec)")
-plot!(ϕ, sol[3,:], lw = 2, line = (:dot, 4) ,label = "Analytical sol")
+plot!(ϕ, jsol[3,:]/uₒ, lw = 2, xlabel = "Time (sec)",label = string("X",3))
+plot!(ϕ, sol[3,:]/Nₒ, lw = 2, line = (:dot, 4) ,ylabel = "N(i,t)/N(1,0)"
+        ,label = string("Analytical sol", "--X",3))
